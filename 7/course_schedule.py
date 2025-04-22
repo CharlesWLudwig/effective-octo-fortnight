be8fe2da0b1953b9a5c26bb5b795711957b0b86d
Charles W Ludwig <124328094+CharlesWLudwig@users.noreply.github.com>
2025-04-22 17:42:44 -0400

    Create course_schedule.py

---
+++ 7/course_schedule.py
@@ -0,0 +1,24 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        in_degree *= numCourses
+        
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            in_degree[course] += 1
+        
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        topological_order = []
+        
+        while queue:
+            node = queue.popleft()
+            topological_order.append(node)
+            
+            for neighbor in graph[node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return topological_order if len(topological_order) == numCourses else []
